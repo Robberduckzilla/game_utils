b30bd960744696939ae50e560116efc91244c264
Rob Drake <50708652+Robberduckzilla@users.noreply.github.com>
2022-06-26 22:20:23 +0100

    Update CopySaves.py

--- CopySaves.py
+++ CopySaves.py
@@ -4,10 +4,10 @@ import shutil
 import time
 
 delay_in_minutes = 0.1
-source = "C:/Users/isabelle/appdata/roaming/obs-studio/"
-dest =  "C:/Users/isabelle/Creative Cloud Files/Initiative Design/Client Work/02_Bristol Adventurers Guild/projects/01 Twitch Charity Stream/obs-backups/"
+source = ""
+dest =  ""
 
-print(f'Backing up OBS stuff every {delay_in_minutes} minutes.')
+print(f'Backing up stuff every {delay_in_minutes} minutes.')
 i=0
 
 while(True):
